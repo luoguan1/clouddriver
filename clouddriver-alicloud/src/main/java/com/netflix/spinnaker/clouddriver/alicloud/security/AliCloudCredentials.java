cc1af21a3603b3bb38a694e1dea7a2e65f80c892
luoguan01 <wb-lg545155@alibaba-inc.com>
2019-07-23 14:38:28 +0800

    feat(alibabacloud): add alicloud provider credential

---
+++ clouddriver-alicloud/src/main/java/com/netflix/spinnaker/clouddriver/alicloud/security/AliCloudCredentials.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2019 Alibaba Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spinnaker.clouddriver.alicloud.security;
+
+import java.util.List;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.netflix.spinnaker.clouddriver.security.AccountCredentials;
+import com.netflix.spinnaker.clouddriver.security.ProviderVersion;
+import com.netflix.spinnaker.fiat.model.resources.Permissions;
+
+public class AliCloudCredentials implements AccountCredentials<AccountCredentials> {
+
+  private static final String CLOUD_PROVIDER = "alicloud";
+
+  private String name;
+
+  private String accessKeyId;
+
+  private String accessSecretKey;
+
+  private List<String> regions;
+
+  private String providerVersion;
+
+  private List<String> requiredGroupMembership;
+
+  private Permissions permissions;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAccessKeyId() {
+    return accessKeyId;
+  }
+
+  public void setAccessKeyId(String accessKeyId) {
+    this.accessKeyId = accessKeyId;
+  }
+
+  public String getAccessSecretKey() {
+    return accessSecretKey;
+  }
+
+  public void setAccessSecretKey(String accessSecretKey) {
+    this.accessSecretKey = accessSecretKey;
+  }
+
+  public void setProviderVersion(String providerVersion) {
+    this.providerVersion = providerVersion;
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String getEnvironment() {
+    return null;
+  }
+
+  @Override
+  public String getAccountType() {
+    return null;
+  }
+
+  @Override
+  @JsonIgnore
+  public AccountCredentials getCredentials() {
+    return null;
+  }
+
+  @Override
+  public String getCloudProvider() {
+    return CLOUD_PROVIDER;
+  }
+
+  @Override
+  public List<String> getRequiredGroupMembership() {
+    return requiredGroupMembership;
+  }
+
+  @Override
+  public Permissions getPermissions() {
+    return permissions;
+  }
+
+  public List<String> getRegions() {
+    return regions;
+  }
+
+  public void setRegions(List<String> regions) {
+    this.regions = regions;
+  }
+}
